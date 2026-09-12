a81fd49249907b05f72350175a0b820292d5bdff
Omer Aboya <omeraboya@gmail.com>
2026-09-12 14:59:10 +0500

    Delete q7.c

--- q7.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    char user_char;
-    printf("Enter a single character: ");
-    user_char = getchar();
-    printf("You Entered:%c\n", user_char);
-    return 0;
-}
